fcb7a3169b0d60986ab11b3296ffaca9de477091
Harold Le <haroldle2735@gmail.com>
2026-05-11 00:44:18 -0400

    Time: 59ms (52.23%) | Memory: 29.55MB (67.83%)

--- 11.Container_With_Most_Water/solution.py
+++ 11.Container_With_Most_Water/solution.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        max_water = 0
+        l, r = 0, len(height) - 1
+        best = 0
 
-        while left < right:
-            width = right - left
-            current_height = min(height[left], height[right])
-            area = width * current_height
-            max_water = max(max_water, area)
+        while l < r:
+            h = min(height[l], height[r])
+            best = max(best, h * (r - l)) #it's right - left, like partition algorithm
 
-            if height[left] < height[right]:
-                left += 1
+            if height[l] < height[r]:
+                l += 1
             else:
-                right -= 1
+                r -= 1
 
-        return max_water
+        return best
