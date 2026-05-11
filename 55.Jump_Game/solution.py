8d035eed7816582a97c4c61b75c85b494c28a110
Harold Le <haroldle2735@gmail.com>
2026-05-10 21:04:35 -0400

    Time: 23ms (46.12%) | Memory: 20.15MB (86.01%)

---
+++ 55.Jump_Game/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        farthest = 0
+
+        for i in range(len(nums)):
+            if i > farthest:
+                return False
+
+            farthest = max(farthest, i + nums[i])
+
+            if farthest >= len(nums) - 1:
+                return True
+
+        return True
+    
+    # farthest = max(farthest, current_index + jump_length)
+    # at each index track the furthest index it can go
