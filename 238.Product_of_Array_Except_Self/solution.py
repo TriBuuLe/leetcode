c0b079f178696078b3acb117ba774e49b1401083
Harold Le <haroldle2735@gmail.com>
2026-05-11 20:52:13 -0400

    Time: 15ms (94.22%) | Memory: 25.46MB (70.26%)

--- 238.Product_of_Array_Except_Self/solution.py
+++ 238.Product_of_Array_Except_Self/solution.py
@@ -1,22 +1,27 @@
 class Solution:
     def productExceptSelf(self, nums):
-        '''
-        [1,2,3,4]
-        [24,12,4,1]
-        [24,12,8,6]
-        '''
         n = len(nums)
         answer = [1] * n
 
+        '''
+        answer = [1,1,1,1]
+        nums = [2,3,4,5]
+
+        prefix = 1
+        i = 0 -> answer: [1,1,1,1] prefix = 2
+        i = 1 -> answer: [1,2,1,1] prefix = 6
+        '''
         prefix = 1
         for i in range(n):
             answer[i] = prefix
             prefix *= nums[i]
-        # answer: [1,1,2,6]
+
         suffix = 1
         for i in range(n - 1, -1, -1):
             answer[i] *= suffix
             suffix *= nums[i]
-        # answer: [24,12,8,6]
+
         return answer
+        
+
 
